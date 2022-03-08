ef161070fc82857d94556115e62690fab0d26570
SerhiyPyatak <serhiypyatak@yahoo.com>
2022-03-08 09:57:41 +0200

    Search form functionality updating started

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import {useState} from 'react';
 import { css } from '@emotion/css/macro';
 import styled from '@emotion/styled';
 import { screenSizes } from '../assets/screenSizes';
 import PropTypes from 'prop-types';
 import useResizeAware from 'react-resize-aware';
+import {FaSearch} from 'react-icons/fa';
+import Typing from './generic/Typing';
 
 // #region constants
 const SECTION_BACKGROUND = '#f5f5f5';
@@ -12,6 +14,12 @@ const INPUT_BACKGROUND = '#fff';
 const INPUT_BORDER = '1px solid rgb(209,209,209)';
 const INPUT_PLACEHOLDER = 'Looking for answer? Just start typing a search term!';
 const INPUT_PLACEHOLDER_PHONE = 'Need answer? Type a search term!';
+
+const initState = {
+	loading: false,
+	query: "",
+	data: [],
+}
 // #endregion
 
 // #region styled-components
@@ -31,16 +39,64 @@ const Input = styled.input`
 	color: #595959;
 	font-size: 16px;
 	padding: 20px 25px;
+	padding-left: 5vw;
 	border: ${INPUT_BORDER};
 	box-shadow: none;
 	position: relative;
 	outline: 0;
 	border-radius: 10vw;
+	box-sizing: border-box;
+
+	@media (max-width: ${screenSizes.largeTablet}) {
+		padding-left: 7vw;
+	};
 
 	@media (max-width: ${screenSizes.mediumTablet}) {
 		width: 80%;
+		padding-left: 9vw;
 	};
-`
+
+	@media (max-width: ${screenSizes.smartPhones}) {
+		padding-right: 0;
+		padding-left: 11vw;
+		width: 90%;
+	}
+`;
+
+const searchIco = css`
+	display: flex;
+    align-items: center;
+    border: none;
+    background: none;
+    position: relative;
+    left: 4%;
+    font-size: 1.3vw;
+	z-index: 1;
+	color: #45454C;
+
+	@media (max-width: ${screenSizes.largeTablet}) {
+		left: 5%;
+		font-size: 2vw;
+	};
+
+	@media (max-width: ${screenSizes.mediumTablet}) {
+		left: 7%;
+		font-size: 3vw;
+	};
+
+	@media (max-width: ${screenSizes.smartPhones}) {
+		left: 10%;
+		font-size: 5vw;
+	};
+`;
+
+const TypeAnim = css`
+	display: flex;
+    align-items: center;
+    width: 3vw;
+    position: relative;
+    left: -5%;
+`;
 // #endregion
 
 // #region functions
@@ -55,15 +111,35 @@ const defaultProps = {};
 /**
  * 
  */
+const TypingAnimation = () => {
+	return <div className={TypeAnim}><Typing /></div>
+}
+
 const SearchForm = () => {
 	const [resizeListener, sizes] = useResizeAware();
+	const [appState, setAppState] = useState(initState);
+
+	const delayStateLoadingFalse = (delay) => {
+        setTimeout(() => setAppState(appSt => ({...appSt, loading: false})), delay);
+    };
+
+	const handleType = ({target}) => {
+		setAppState(appSt => ({...appSt, loading: true}));
+		setAppState(appSt => ({...appSt, query: target.value}));
+		// ToDo: getting filtered articles
+		delayStateLoadingFalse(2000);
+	}
+
 	return <Section>
 		{resizeListener}
+		<button className={searchIco} type="submit" disabled><FaSearch /> </button>	
 		<Input
 			placeholder={
-				sizes.width <= parseInt(screenSizes.smartPhones) ? INPUT_PLACEHOLDER_PHONE : INPUT_PLACEHOLDER
+				sizes.width <= parseInt(screenSizes.mediumTablet) ? INPUT_PLACEHOLDER_PHONE : INPUT_PLACEHOLDER
 			}
+			onChange={handleType}
 		/>
+		{ appState.loading && <TypingAnimation /> }
 	</Section>;
 }
 
