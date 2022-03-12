4ecd87e88ff9f219bb92cd15cb46eaaa45bace3c
SerhiyPyatak <serhiypyatak@yahoo.com>
2022-03-12 12:25:50 +0200

    Live search functionality completed

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -4,9 +4,10 @@ import styled from '@emotion/styled';
 import { screenSizes } from '../assets/screenSizes';
 import PropTypes from 'prop-types';
 import useResizeAware from 'react-resize-aware';
-import {FaSearch, FaRegHandPointer, FaRegEnvelope} from 'react-icons/fa';
+import {FaSearch, FaRegHandPointer, FaRegEnvelope, FaFilm, FaRegFileAlt} from 'react-icons/fa';
 import Typing from './generic/Typing';
 import {vwContentIdx} from '../assets/apisimul/serverdata_main';
+import Publication from './generic/Publication';
 
 // #region constants
 const SECTION_BACKGROUND = '#f5f5f5';
@@ -98,6 +99,46 @@ const TypeAnim = css`
     position: absolute;
     left: 79%;
     top: 13.8vw;
+
+	@media (max-width: 1270px) {
+		width: 3.2vw;
+        left: 77%;
+		top: 14.8vw;
+	};
+
+	@media (max-width: 1020px) {
+		width: 4vw;
+		left: 75%;
+		top: 16.3vw;
+	};
+
+	@media (max-width: ${screenSizes.largeTablet}) {
+		width: 5vw;
+		top: 19vw;
+	};
+
+	@media (max-width: ${screenSizes.mediumTablet}) {
+		width: 6vw;
+		left: 80%;
+		top: 36vw;
+	};
+
+	@media (max-width: 538px) {
+		width: 8vw;
+		left: 78%;
+		top: 38vw;
+	};
+
+	@media (max-width: ${screenSizes.smartPhones}) {
+		width: 9vw;
+		left: 82%;
+		top: 48vw;
+	};
+
+	@media (max-width: 350px) {
+		width: 11vw;
+		top: 51vw;
+	};
 `;
 
 const liveSearchContainer = css`
@@ -110,6 +151,9 @@ const liveSearchContainer = css`
 	box-sizing: border-box;
 	padding: 0;
 	font-family: "Open Sans",Helvetica,Arial,sans-serif;
+	max-height: 55%;
+    overflow-y: scroll;
+	overflow-x: hidden;
 
 	& > li {
 		display: flex;
@@ -117,13 +161,55 @@ const liveSearchContainer = css`
 		list-style: none;
 		padding: 0.8% 2%;
 		color: #45454C;
-	}
+	};
+
+	@media (max-width: 1270px) {
+		top: 17vw;
+		left: 20.8%;
+		width: 61%;
+	};
+	@media (max-width: 1020px) {
+		top: 19vw;
+		left: 21%;
+		width: 60.7%;
+	};
+	@media (max-width: ${screenSizes.largeTablet}) {
+		top: 22vw;
+		left: 21.5%;
+		width: 60.5%;
+		max-height: 70%;
+	};
+	@media (max-width: ${screenSizes.mediumTablet}) {
+		top: 39.4vw;
+		left: 15.5%;
+		width: 74.5%;
+	};
+	@media (max-width: 538px) {
+		top: 43.5vw;
+		left: 16.5%;
+		width: 72%;
+		padding: 2% 3%;
+	};
+	@media (max-width: ${screenSizes.smartPhones}) {
+		top: 53.5vw;
+		left: 13.5%;
+		width: 81%;
+	};
+	@media (max-width: 350px) {
+		top: 59vw;
+		left: 15.5%;
+		width: 77%;
+	};
 `;
 
 const keepTypeTip = css`
 	justify-content: center;
 	color: #a03717 !important;
 	gap: 1%;
+
+	@media (max-width: 538px) {margin-bottom: 3vw};
+	@media (max-width: ${screenSizes.smartPhones}) {font-size: 3.2vw};
+	@media (max-width: 350px) {font-size: 3.6vw};
 `;
 
 const contactLiveSearch = css`
@@ -142,11 +228,110 @@ const contactLiveSearch = css`
 		padding: 0.5% 0.8%;
 		border-radius: 1vw;
 		box-sizing: border-box;
+
+		@media (max-width: 1270px) {width: 14%};
+		@media (max-width: ${screenSizes.largeTablet}) {width: 16%};
+		@media (max-width: ${screenSizes.mediumTablet}) {width: 20%};
+		@media (max-width: 538px) {width: 30%};
+		@media (max-width: 350px) {width: 40%};
 	};
 
 	& > a:hover {
 		background-color: #4a4a4a;
 	};
+
+	@media (max-width: 1270px) {font-size: 1.2vw};
+	@media (max-width: ${screenSizes.largeTablet}) {font-size: 1.5vw};
+	@media (max-width: ${screenSizes.mediumTablet}) {font-size: 2.3vw};
+	@media (max-width: 538px) {font-size: 2.7vw};
+	@media (max-width: ${screenSizes.smartPhones}) {font-size: 3.2vw};
+	@media (max-width: 350px) {font-size: 3.6vw};
+`;
+
+const liveSearchItm = css`
+	display: flex;
+	flex-direction: column;
+
+	&:hover {
+		background-color: #e6cc8f;
+		color: #a03717;
+	};
+
+	& > div {
+		width: 100%;
+	};
+
+	@media (max-width: 538px) {
+		margin-bottom: 1.4vw;
+	}
+`;
+
+const liveSearchItmCategory = css`
+	font-style: italic;
+	font-size: 0.8vw;
+	color: #878787;
+	padding-left: 3.5vw;
+
+	@media (max-width: 1270px) {
+		font-size: 0.9vw;
+		padding-left: 4vw;
+	};
+
+	@media (max-width: ${screenSizes.largeTablet}) {
+		font-size: 1.2vw;
+		padding-left: 6vw;
+	};
+
+	@media (max-width: ${screenSizes.mediumTablet}) {
+		font-size: 1.7vw;
+		padding-left: 7vw;
+	};
+
+	@media (max-width: 538px) {
+		font-size: 2.3vw;
+		padding-left: 11vw;
+	};
+
+	@media (max-width: ${screenSizes.smartPhones}) {
+		font-size: 2.8vw;
+		padding-left: 13vw;
+	};
+
+	@media (max-width: 350px) {
+		font-size: 3.4vw;
+		padding-left: 15vw;
+	};
+`;
+
+const publication = css`
+	display: flex;
+	flex-direction: row;
+	gap: 0.5vw;
+
+	@media (max-width: ${screenSizes.largeTablet}) {gap: 1vw};
+	@media (max-width: 538px) {
+		gap: 2vw;
+	};
+`;
+
+const publicationIcon = css`
+	font-size: 1.3vw;
+
+	@media (max-width: 1270px) {font-size: 1.5vw};
+	@media (max-width: ${screenSizes.largeTablet}) {font-size: 1.8vw};
+	@media (max-width: ${screenSizes.mediumTablet}) {font-size: 2.5vw};
+	@media (max-width: 538px) {font-size: 3.6vw};
+	@media (max-width: ${screenSizes.smartPhones}) {font-size: 4.2vw};
+	@media (max-width: 350px) {font-size: 5.2vw};
+`;
+
+const publicationTitle = css`
+	@media (max-width: 1270px) {font-size: 1.2vw};
+	@media (max-width: ${screenSizes.largeTablet}) {font-size: 1.5vw};
+	@media (max-width: ${screenSizes.mediumTablet}) {font-size: 2.3vw};
+	@media (max-width: 538px) {font-size: 2.7vw};
+	@media (max-width: ${screenSizes.smartPhones}) {font-size: 3.2vw};
+	@media (max-width: 350px) {font-size: 3.6vw};
 `;
 // #endregion
 
@@ -163,7 +348,8 @@ const defaultProps = {};
  *  This is components compound. In addition to form component itself contains:
  *  - <TypingAnimation> component: displays while user typing a search query. Two seconds
  *   after user stops typing this component has to be hidden again
- *  - <QueryLiveSearch>: a box displaying search results while user typing a                                  query.   
+ *  - <QueryLiveSearch>: a box displaying search results while user typing a                                  query.  
+ *  - <LiveSearchItem> component: displays a position inside QueryLiveSearch corresponds to item being found
  *  - <ContactInvite>: default nested "search result" contains proposition for user to fill contact form
  */
 const TypingAnimation = () => {
@@ -180,8 +366,27 @@ const ContactInvite = () => {
 			</a>
 		</li>
 	)
-}
+};
 
+const LiveSearchItem = ({content, category}) => {
+	return (
+		<li className={liveSearchItm}>
+			<Publication 
+				title = {content.title} 
+				cssLookup = {{
+					container: publication,
+					title: publicationTitle,
+				}}>
+				{
+					content.video ?
+					<FaFilm className={publicationIcon} />
+					: <FaRegFileAlt className={publicationIcon}	/>
+				}
+			</Publication>
+			<div className={liveSearchItmCategory}>Category: {category}</div>
+		</li>
+	);
+};
 
 const QueryLiveSearch = ({isTyping, data}) => {
 	return (
@@ -189,14 +394,13 @@ const QueryLiveSearch = ({isTyping, data}) => {
 			{isTyping && <li className={keepTypeTip}><FaRegHandPointer /> Keep typing for live search results...</li>}
 			{
 				!data.length ? <li>--- No results found ---</li> 
-				: data.map((category) => category.view.map((article) => <li>{article.title}</li>))
+				: data.map((category) => category.view.map((article) => <LiveSearchItem content={article} category={category.category} />))
 				
 			}
 			<ContactInvite />
 		</ul>
 	)
 };
-
 
 const SearchForm = () => {
 	const [resizeListener, sizes] = useResizeAware();
