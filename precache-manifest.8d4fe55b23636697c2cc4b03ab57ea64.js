260da0c16756f52320a14b0fdf9396ab86fad10f
SerhiyPyatak <serhiipyatak@gmail.com>
2022-06-21 11:31:24 +0300

    Updates

---
+++ precache-manifest.8d4fe55b23636697c2cc4b03ab57ea64.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e5154991b762b56f45",
+    "url": "/ttt-knowdb-deploy/static/css/main.9bc3c242.chunk.css"
+  },
+  {
+    "revision": "89e5154991b762b56f45",
+    "url": "/ttt-knowdb-deploy/static/js/main.89e51549.chunk.js"
+  },
+  {
+    "revision": "81c755ae00733e9792ac",
+    "url": "/ttt-knowdb-deploy/static/js/1.81c755ae.chunk.js"
+  },
+  {
+    "revision": "1ac67204e7cd7bdd81bc",
+    "url": "/ttt-knowdb-deploy/static/js/runtime~main.1ac67204.js"
+  },
+  {
+    "revision": "a51a8249e13ecae6f067cbcb393eec2b",
+    "url": "/ttt-knowdb-deploy/static/media/IBS Logo.a51a8249.png"
+  },
+  {
+    "revision": "a7ca3eae6f525e8173ec00d1b059bb7e",
+    "url": "/ttt-knowdb-deploy/static/media/purina_title-sharpen.a7ca3eae.png"
+  },
+  {
+    "revision": "a4c7885881adb4d2e716939391d673ff",
+    "url": "/ttt-knowdb-deploy/static/media/coming_soon.a4c78858.jpg"
+  },
+  {
+    "revision": "f7fa61d911d2485a350858e756e16ce4",
+    "url": "/ttt-knowdb-deploy/static/media/nestle_logo.f7fa61d9.png"
+  },
+  {
+    "revision": "935250149d4c5a67ff8d3b367d0b5fa8",
+    "url": "/ttt-knowdb-deploy/index.html"
+  }
+];
